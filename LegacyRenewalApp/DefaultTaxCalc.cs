f4ea7710dca01ab6c3db9d13ba865b21d678dd6c
Tymon Wadecki <s31837@pjwstk.edu.pl>
2026-04-14 15:49:59 +0200

    stworzenie iterfejsu oraz klasy odpowiedzialniej za wyliczanie konkretnego podatku dla podanego w kraju

---
+++ LegacyRenewalApp/DefaultTaxCalc.cs
@@ -0,0 +1,20 @@
+﻿namespace LegacyRenewalApp;
+
+public class DefaultTaxCalc:ITaxCalc
+{
+    public decimal CalculateTaxAmount(string country, decimal taxBaseAmount)
+    {
+        decimal taxRate = 0.20m;
+        if (country == "Poland")
+            taxRate = 0.23m;
+        else if (country == "Germany")
+            taxRate = 0.19m;
+        else if (country == "Czech Republic")
+            taxRate = 0.21m;
+        else if (country == "Norway")
+            taxRate = 0.25m;
+        
+        return taxRate * taxBaseAmount;
+        
+    }
+}
---
+++ LegacyRenewalApp/ITaxCalc.cs
@@ -0,0 +1,6 @@
+﻿namespace LegacyRenewalApp;
+
+public interface ITaxCalc
+{
+    decimal CalculateTaxAmount(string country,decimal taxBaseAmount);
+}
