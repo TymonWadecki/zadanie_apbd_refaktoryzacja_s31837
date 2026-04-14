5e6905a0dc8fa6bfd403acbcf2f5fba9c2827925
Tymon Wadecki <s31837@pjwstk.edu.pl>
2026-04-14 22:05:04 +0200

    Interfejs oraz klasy dodatkowe potrzebne do usuniecia wielokrotnych instrukcji warunkowych w metodzie CreateRenewalInvoice

---
+++ LegacyRenewalApp/IDiscountRule.cs
@@ -0,0 +1,11 @@
+﻿namespace LegacyRenewalApp;
+
+public interface IDiscountRule
+{
+    (decimal Amount, string Note) Calculate(
+        Customer customer, 
+        SubscriptionPlan plan, 
+        int seatCount, 
+        decimal baseAmount, 
+        bool useLoyaltyPoints);
+}
---
+++ LegacyRenewalApp/LoyaltyYearsDiscountRule.cs
@@ -0,0 +1,11 @@
+﻿namespace LegacyRenewalApp;
+
+public class LoyaltyYearsDiscountRule:IDiscountRule
+{
+    public (decimal Amount, string Note) Calculate(Customer customer, SubscriptionPlan plan, int seatCount, decimal baseAmount, bool useLoyaltyPoints)
+    {
+        if (customer.YearsWithCompany >= 5) return (baseAmount * 0.07m, "long-term loyalty discount; ");
+        if (customer.YearsWithCompany >= 2) return (baseAmount * 0.03m, "basic loyalty discount; ");
+        return (0m, string.Empty);
+    }
+}
